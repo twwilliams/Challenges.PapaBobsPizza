202f9743fafa2d334454232da8e0d944531a7b4d
Tommy Williams <tommy@sapid.com>
2018-02-11 23:46:51 -0700

    Solved

---
+++ ChallengeFirstPapaBobsWebsite/Default.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeFirstPapaBobsWebsite.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        .auto-style2 {
+            color: #FF0000;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1 style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">
+                <asp:Image ID="Image1" runat="server" CssClass="auto-style1" ImageUrl="~/img/PapaBob.png" />
+                <span class="auto-style1">Papa Bob&rsquo;s Pizza and Software</span></h1>
+        </div>
+        <asp:RadioButton ID="babyBobRadioButton" runat="server" Text="Baby Bob Size (10&quot;) &ndash; $10.00" GroupName="Size" />
+        <br />
+        <asp:RadioButton ID="mamaBobRadioButton" runat="server" Text="Mama Bob Size (13&quot;) &ndash; $13.00" GroupName="Size" />
+        <br />
+        <asp:RadioButton ID="papaBobRadioButton" runat="server" Text="Papa Bob Size (16&quot;) &ndash; $16.00" GroupName="Size" />
+        <br />
+        <br />
+        <asp:RadioButton ID="thinCrustRadioButton" runat="server" Text="Thin Crust" GroupName="Crust" />
+        <br />
+        <asp:RadioButton ID="deepDishRadioButton" runat="server" Text="Deep Dish (+$2.00)" GroupName="Crust" />
+        <br />
+        <br />
+        <asp:CheckBox ID="pepperoniCheckBox" runat="server" Text="Pepperoni (+$1.50)" />
+        <br />
+        <asp:CheckBox ID="onionsCheckBox" runat="server" Text="Onions (+$0.75)" />
+        <br />
+        <asp:CheckBox ID="greenPeppersCheckBox" runat="server" Text="Green Peppers (+$0.50)" />
+        <br />
+        <asp:CheckBox ID="redPeppersCheckBox" runat="server" Text="Red Peppers (+$0.75)" />
+        <br />
+        <asp:CheckBox ID="anchoviesCheckBox" runat="server" Text="Anchovies (+$2.00)" />
+        <br />
+        <br />
+        <h2 class="auto-style1">Papa Bob&rsquo;s <span class="auto-style2">Special Deal</span></h2>
+        <p>Save $2.00 when you add Pepperoni, Green Peppers, and Anchovies OR Pepperoni, Red Peppers, and Onions to your pizza.</p>
+        <p>
+            <asp:Button ID="purchaseButton" runat="server" Text="Purchase" OnClick="purchaseButton_Click" />
+        </p>
+        <p>
+            <asp:Label ID="totalLabel" runat="server"></asp:Label>
+        </p>
+        <p>Sorry. At this time, you can only order one pizza online and for pickup only. We need a better web site!</p>
+    </form>
+</body>
+</html>
---
+++ ChallengeFirstPapaBobsWebsite/Default.aspx.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeFirstPapaBobsWebsite
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void purchaseButton_Click(object sender, EventArgs e)
+        {
+            totalLabel.ForeColor = System.Drawing.Color.Black;
+            totalLabel.Text = "";
+
+            decimal total = 0m;
+
+            Toppings toppings = Toppings.None;
+
+            if (babyBobRadioButton.Checked)
+            {
+                total += 10;
+            }
+            else if (mamaBobRadioButton.Checked)
+            {
+                total += 13;
+            } else if (papaBobRadioButton.Checked)
+            {
+                total += 16;
+            }
+            else
+            {
+                ValidationError("You must select a size");
+                return;
+            }
+
+            if (thinCrustRadioButton.Checked)
+            {
+                total += 0;
+            }
+            else if (deepDishRadioButton.Checked)
+            {
+                total += 2;
+            }
+            else
+            {
+                ValidationError("You must select a crust");
+                return;
+            }
+
+            if (pepperoniCheckBox.Checked)
+            {
+                total += 1.5m;
+                toppings |= Toppings.Pepperoni;
+            }
+            if (onionsCheckBox.Checked)
+            {
+                total += 0.75m;
+                toppings |= Toppings.Onions;
+            }
+            if (greenPeppersCheckBox.Checked)
+            {
+                total += 0.50m;
+                toppings |= Toppings.GreenPeppers;
+            }
+            if (redPeppersCheckBox.Checked)
+            {
+                total += 0.75m;
+                toppings |= Toppings.RedPeppers;
+            }
+            if (anchoviesCheckBox.Checked)
+            {
+                total += 2.00m;
+                toppings |= Toppings.Anchovies;
+            }
+
+            if (
+                (toppings.HasFlag(Toppings.Pepperoni)
+                && toppings.HasFlag(Toppings.GreenPeppers)
+                && toppings.HasFlag(Toppings.Anchovies))
+                ||
+                (toppings.HasFlag(Toppings.Pepperoni)
+                && toppings.HasFlag(Toppings.RedPeppers)
+                && toppings.HasFlag(Toppings.Onions))
+                )
+            {
+                total -= 2.00m;
+            }
+
+            totalLabel.Text = $"Total: {total:c}";
+        }
+
+        private void ValidationError(string message)
+        {
+            totalLabel.ForeColor = System.Drawing.Color.Red;
+            totalLabel.Text = message;
+        }
+    }
+
+    [Flags]
+    public enum Toppings
+    {
+        None = 0,
+        Pepperoni = 1,
+        Onions = 2,
+        GreenPeppers = 4,
+        RedPeppers = 8,
+        Anchovies = 16
+    }
+}
